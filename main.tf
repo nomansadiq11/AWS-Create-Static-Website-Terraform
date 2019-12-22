a702f2999e2b1cf0c480afa02c4187513a746844
Noman Sadiq <nomi.cs786@gmail.com>
2019-12-22 12:36:18 +0400

    added static website to create

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+
+resource "aws_s3_bucket" "teststaticwebsite" {
+  bucket = "nomansadiq11"
+  acl    = "public-read"
+  policy = "${file("policy.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
