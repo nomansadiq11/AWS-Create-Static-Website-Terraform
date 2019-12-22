4cc2f15425fb7987cb7af85bb7e4743d6e24f271
Noman Sadiq <nomi.cs786@gmail.com>
2019-12-22 12:44:34 +0400

    remove rules

--- main.tf
+++ main.tf
@@ -9,15 +9,5 @@ resource "aws_s3_bucket" "teststaticwebsite" {
     index_document = "index.html"
     error_document = "error.html"
 
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
   }
 }
